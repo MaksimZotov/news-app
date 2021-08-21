e14c41518ae1a9b1f5c309e9ee7f296fd78a1041
MaksimZotov <zotovmaksim1254@gmail.com>
2021-08-21 13:25:50 +0300

    added a comment

--- app/src/main/java/com/maksimzotov/news/domain/entities/NewsWrapper.kt
+++ app/src/main/java/com/maksimzotov/news/domain/entities/NewsWrapper.kt
@@ -1,5 +1,12 @@
 package com.maksimzotov.news.domain.entities
 
+// This wrapper is implemented to get a list of news via retrofit.
+// Using of a custom deserializer for a generic type (in our case, List<NewsItem>) was failed.
+// Passing the generic type
+// val type = object : TypeToken<List<NewsItem>> () {}.type
+// to registerTypeAdapter(type, ...) in GsonBuilder did not help - the application was crashed.
+// So I had to add the wrapper
+
 data class NewsWrapper(
     val news: List<NewsItem>
 )
